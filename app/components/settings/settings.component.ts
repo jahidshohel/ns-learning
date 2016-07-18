afeda3cfa5e8ca2fae4249ca41f96c24244d5fc9
jshohel <jshohel@gmail.com>
2016-07-18 18:42:59 +0200

    added settings

--- app/components/settings/settings.component.ts
+++ app/components/settings/settings.component.ts
@@ -7,9 +7,17 @@ import {Router} from "@angular/router";
     styleUrls: ["components/settings/settings-common.css", "components/settings/settings.css"]
 })
 export class SettingsComponent {
+    //https://github.com/jlooper/practice-buddy-mobile-app-2.0/blob/master/app/components/settings-detail/settings-detail.component.ts
+    instruments = [];
 
-    constructor(private _router: Router){}
-    onBackTap(){
+    constructor(private _router:Router) {
+        this.instruments = ['acoustic guitar', 'banjo', 'cello',
+            'clarinet', 'elecric guitar', 'flute', 'french horn',
+            'handbells', 'harp', 'mandolin', 'music', 'oboe', 'organ', 'percussion', 'piano', 'sax',
+            'trombone', 'trumpet', 'violin', 'voice', 'xylophone'];
+    }
+
+    onBackTap() {
         this._router.navigate([""]);
     }
 }
